b611ecc99605203159d107939d7133878bd18678
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2020-01-23 17:50:50 +0700

    btn insert

--- app/src/main/java/com/aas/siperpus/MainActivity.java
+++ app/src/main/java/com/aas/siperpus/MainActivity.java
@@ -2,13 +2,34 @@ package com.aas.siperpus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Deklarasi
+    private TextView tvData;
+    private Button btnInsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Koneksi dengan layout
+        tvData = findViewById(R.id.tv_hasil);
+        btnInsert = findViewById(R.id.btn_insert);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Membuka activity insert
+                Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
